fe062ab8359b914eefec8e3f7bf30a95f80cdf8a
ceuli <ceuli.khuat@ewoosoft.com>
2021-03-25 11:08:26 +0700

    update music

--- src/components/MusicBackground.tsx
+++ src/components/MusicBackground.tsx
@@ -1,19 +1,20 @@
 import React, { useEffect } from 'react';
 import { Howl } from 'howler';
+import { SoundOutlined, SoundFilled } from '@ant-design/icons';
+import { Button } from 'antd';
 
 const sound = new Howl({
   src: [
-    '/music/Blue Boi.mp3',
-    '/music/Chill Day.mp3',
+    '/music/Comethru.mp3',
+    '/music/You_re Beautiful.mp3',
     '/music/City Of Angels.mp3',
-    '/music/Good Morning.mp3',
-    '/music/In My Dreams.mp3',
-    '/music/Me 2 (Feat. Julian Avila).mp3',
-    '/music/Summertime Love.mp3',
-    '/music/The Process.mp3',
-    '/music/musicBackground.mp3',
+    // '/music/Good Morning.mp3',
+    // '/music/In My Dreams.mp3',
+    // '/music/Me 2 (Feat. Julian Avila).mp3',
+    // '/music/Summertime Love.mp3',
+    // '/music/The Process.mp3',
+    // '/music/musicBackground.mp3',
   ],
-  autoplay: true,
   loop: true,
   volume: 0.5,
 
@@ -21,11 +22,21 @@ const sound = new Howl({
 });
 
 const MusicBackground = () => {
-  useEffect(() => {
-    // console.log('ádfasdf', sound);
-    sound.play();
-  }, []);
-  return <></>;
+  return (
+    <div style={{ position: 'absolute', top: 20, left: 20, zIndex: 1 }}>
+      <Button
+        // type='primary'
+        shape='circle'
+        icon={sound.playing() ? <SoundFilled /> : <SoundOutlined />}
+        onClick={() => {
+          // console.log('sound', sound);
+          if (sound.playing()) {
+            sound.pause();
+          } else sound.play();
+        }}
+      />
+    </div>
+  );
 };
 
 export default MusicBackground;
